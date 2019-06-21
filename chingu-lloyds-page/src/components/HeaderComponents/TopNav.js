678942ac9ee1175ec1a8f58c226ccfd97d9734c2
MiriamPeskowitz <miriam5000@gmail.com>
2019-06-21 15:47:36 -0400

    Ready for popup

--- chingu-lloyds-page/src/components/HeaderComponents/TopNav.js
+++ chingu-lloyds-page/src/components/HeaderComponents/TopNav.js
@@ -7,7 +7,6 @@ const TopNav = () => {
   return (
     <nav className="top-nav-container">
       <TopNavLloydsLogo />
-      <div className="ul-container">
       <ul className="nav1">
         <li><a href="https://www.lloyds.com/about-lloyds" target="_blank" rel="noopener noreferrer" >About</a></li>
         
@@ -20,7 +19,7 @@ const TopNav = () => {
         <li><a href="https://www.lloyds.com/contact-us" alt="Contact"> Contact</a></li>
         <li><a href="https://secure.lloyds.com/login?ReturnUrl=%2f%3fwa%3dwsignin1.0%26wtrealm%3dhttps%253a%252f%252fwww.lloyds.com%252f%26wctx%3drm%253d0%2526id%253d82f189f3-737f-45bb-b4bc-d2a470ec0ac0%2526ru%253dhttps%25253a%25252f%25252fwww.lloyds.com%25252fcontact-us%26wct%3d2019-06-16T03%253a26%253a09Z&wa=wsignin1.0&wtrealm=https%3a%2f%2fwww.lloyds.com%2f&wctx=rm%3d0%26id%3d82f189f3-737f-45bb-b4bc-d2a470ec0ac0%26ru%3dhttps%253a%252f%252fwww.lloyds.com%252fcontact-us&wct=2019-06-16T03%3a26%3a09Z" target="_blank" rel="noopener noreferrer" alt="Login or register"> Login/register</a></li>
       </ul>
-      </div>
+   
       <SearchBar /> 
     </nav>
   ) 
